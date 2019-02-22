2f039c945f6d56fe0d7eb30c149f065dc5e7747d
github username <github email address>
2019-02-22 16:49:17 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 class Artist
-  extend Memorable, Findable
-  include Paramable
+  extend Memorable::ClassMethods, Findable
+  include Paramable, Memorable::InstanceMethods
 
   attr_accessor :name
   attr_reader :songs
@@ -10,7 +10,8 @@ class Artist
   @@artists = []
 
   def initialize
-    @@artists << self
+    #@@artists << self
+    super 
     @songs = []
   end
 
